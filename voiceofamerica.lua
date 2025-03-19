d6e549b232c47e26e88b4bf4877ea0081937bf53
arkiver <arkiver@protonmail.com>
2025-03-19 12:32:24 +0100

    Version 20250319.05. Set max tries to 3.

--- voiceofamerica.lua
+++ voiceofamerica.lua
@@ -648,7 +648,7 @@ wget.callbacks.httploop_result = function(url, err, http_stat)
     io.stdout:write("Server returned bad response. ")
     io.stdout:flush()
     tries = tries + 1
-    local maxtries = 11
+    local maxtries = 3
     if status_code == 404 then
       tries = maxtries + 1
     end
