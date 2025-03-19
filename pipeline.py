04acb5b56a4758f8eccd71a4cf5ea2f56996b246
arkiver <arkiver@protonmail.com>
2025-03-19 12:29:39 +0100

    Version 20250319.04. Force multi item size as 1 for now.

--- pipeline.py
+++ pipeline.py
@@ -77,11 +77,11 @@ if not WGET_AT:
 #
 # Update this each time you make a non-cosmetic change.
 # It will be added to the WARC files and reported to the tracker.
-VERSION = '20250319.03'
+VERSION = '20250319.04'
 USER_AGENT = 'Mozilla/5.0 (X11; Linux i686; rv:124.0) Gecko/20100101 Firefox/124.0'
 TRACKER_ID = 'voiceofamerica'
 TRACKER_HOST = 'legacy-api.arpa.li'
-MULTI_ITEM_SIZE = 100
+MULTI_ITEM_SIZE = 1 # KEEP ONE
 
 
 ###########################################################################
