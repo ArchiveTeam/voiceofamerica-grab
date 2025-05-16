5a91288567223e555fa9cacdc36e1a19fa117a77
arkiver <arkiver@protonmail.com>
2025-05-16 13:37:44 +0200

    Version 20250516.01. Switch things around.

--- pipeline.py
+++ pipeline.py
@@ -78,8 +78,8 @@ if not WGET_AT:
 #
 # Update this each time you make a non-cosmetic change.
 # It will be added to the WARC files and reported to the tracker.
-VERSION = '20250411.01'
-USER_AGENT = 'Archive Team (https://wiki.archiveteam.org/; email archiveteam@archiveteam.org)'
+VERSION = '20250516.01'
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:136.0) Gecko/20100101 Firefox/136.0'
 TRACKER_ID = 'voiceofamerica'
 TRACKER_HOST = 'legacy-api.arpa.li'
 MULTI_ITEM_SIZE = 100
@@ -260,8 +260,7 @@ class WgetArgs(object):
             '--warc-header', 'operator: Archive Team',
             '--warc-header', 'x-wget-at-project-version: ' + VERSION,
             '--warc-header', 'x-wget-at-project-name: ' + TRACKER_ID,
-            '--warc-dedup-url-agnostic',
-            '--secure-protocol', 'TLSv1_2'
+            '--warc-dedup-url-agnostic'
         ]
 
         with open('voasites.txt', 'r') as f:
