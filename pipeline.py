c46655d6926bdca0c7deb8a41616ca4b0061adc6
arkiver <arkiver@protonmail.com>
2025-05-23 17:31:34 +0200

    Version 20250523.01. Set Archive Team user agent.

--- pipeline.py
+++ pipeline.py
@@ -78,8 +78,8 @@ if not WGET_AT:
 #
 # Update this each time you make a non-cosmetic change.
 # It will be added to the WARC files and reported to the tracker.
-VERSION = '20250521.01'
-USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:136.0) Gecko/20100101 Firefox/136.0'
+VERSION = '20250523.01'
+USER_AGENT = 'Archive Team (https://wiki.archiveteam.org/; email archiveteam@archiveteam.org)'
 TRACKER_ID = 'voiceofamerica'
 TRACKER_HOST = 'legacy-api.arpa.li'
 MULTI_ITEM_SIZE = 100
